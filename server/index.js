01813a884f6a515e2be251a166f71e21398a0b8d
Souradeep Nanda <souradeep.nanda123@gmail.com>
2019-08-05 10:01:52 +0530

    Module is now executable

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+#!/usr/bin/env node
+
 const hcl = require('gopher-hcl');
 const fs = require('fs');
 const express = require('express');
+const path = require('path');
 
 const { transformer } = require('./helpers');
 
@@ -9,10 +12,16 @@ const port = 3001;
 
 app.get('/api/graph', (req, res) => {
   console.log('/api/graph');
-  const source = fs.readFileSync('./src/server/scenarios/01_basic/hcl/entry.hcl');
+  const source = fs.readFileSync(path.join(__dirname, 'scenarios/01_basic/hcl/entry.hcl'));
   const result = hcl.parse(source);
   const transformedGraph = transformer(result);
   res.send(transformedGraph);
 });
+
+app.use('/', express.static(path.join(__dirname, '../', 'build')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
