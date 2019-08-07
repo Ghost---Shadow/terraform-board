648a6aa6162baf894a45c4fea7df6ab985726e38
Souradeep Nanda <souradeep.nanda123@gmail.com>
2019-08-07 13:33:40 +0530

    Added a story to TitleBar

--- src/components/TitleBar/TitleBar.stories.js
+++ src/components/TitleBar/TitleBar.stories.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import TitleBar from './index';
+import SettingsWindow from '../SettingsWindow';
+import PlanAndApply from '../PlanAndApply';
+import RenderWindow from '../RenderWindow';
+
+const bigSample = require('../../../server/scenarios/02_vault/json/vault-cluster/total.json');
 
 const contents = [
   { title: 'Settings', component: <div>Settings component</div> },
@@ -10,8 +15,16 @@ const contents = [
   { title: 'Plan and Apply', component: <div>Plan and Apply component</div> },
 ];
 
+const filled = [
+  { title: 'Settings', component: <SettingsWindow /> },
+  { title: 'Graph', component: <RenderWindow elements={bigSample} /> },
+  { title: 'Plan and Apply', component: <PlanAndApply /> },
+];
+
 storiesOf('TitleBar', module)
   .addDecorator(withKnobs)
   .add('default', () => (
     <TitleBar contents={contents} />
+  )).add('filled', () => (
+    <TitleBar contents={filled} />
   ));
