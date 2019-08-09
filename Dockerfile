9d3a9b011db54adc979a81e9ae6def71a761dc20
Souradeep Nanda <souradeep.nanda123@gmail.com>
2019-08-09 10:33:52 +0530

    Update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,8 @@ RUN apt-get update -y && apt-get upgrade -y
 
 RUN apt-get install git
 
-RUN npm i -g terraform-board
+RUN npm i -g https://github.com/Ghost---Shadow/terraform-board.git
 
-EXPOSE 3001
 ENV AWS_CRED_DIR "~/.aws"
 ENV SSH_CRED_DIR "~/.ssh"
 ENV TF_STATE_DIR './.tfstate'
@@ -15,8 +14,34 @@ VOLUME AWS_CRED_DIR
 VOLUME SSH_CRED_DIR
 VOLUME TF_STATE_DIR
 
-ENV WORK_DIR "/workdir"
-RUN mkdir /workdir
-WORKDIR /workdir
+ENV WORK_DIR "root/workdir"
+RUN mkdir root/workdir
+WORKDIR root/workdir
+
+# Authorize SSH Host
+RUN mkdir -p ~/.ssh && \
+    chmod 700 ~/.ssh && \
+    ssh-keyscan github.com > ~/.ssh/known_hosts
+
+# Add the keys and set permissions
+RUN echo "PLACEHOLDER" > ~/.ssh/id_rsa && \
+    echo "PLACEHOLDER" > ~/.ssh/id_rsa.pub && \
+    chmod 600 ~/.ssh/id_rsa && \
+    chmod 600 ~/.ssh/id_rsa.pub
+
+# Authorize AWS Credentials
+RUN mkdir -p ~/.aws && \
+    chmod 700 ~/.aws
+
+RUN echo "PLACEHOLDER" > ~/.aws/credentials && \
+    chmod 600 ~/.aws/credentials
+
+EXPOSE 3001
+
+# https://serverfault.com/questions/189070/what-firewall-ports-need-to-be-open-to-allow-access-to-external-git-repositories
+EXPOSE 22
+EXPOSE 9418
+EXPOSE 80
+EXPOSE 443
 
 CMD ["terraform-board"]
--- server/routes/settings/index.js
+++ server/routes/settings/index.js
@@ -6,9 +6,9 @@ const childProcess = require('child_process');
 const writeFile = promisify(fs.writeFile);
 const exec = promisify(childProcess.exec);
 
-const decodeAndSave = async (base64, directory) => {
+const decodeAndSave = async (base64, directory, filename) => {
   const buffer = Buffer.from(base64.replace(/data:.*base64,/, ''), 'base64');
-  await writeFile(path.join(directory, 'credentials'), buffer);
+  await writeFile(path.join(directory, filename), buffer);
 };
 
 // For development
@@ -18,7 +18,7 @@ const uploadAwsCredentials = async (req, res) => {
   console.log('/api/credentials/aws');
   try {
     const payload = req.body;
-    await decodeAndSave(payload.base64, process.env.AWS_CRED_DIR || sandbox);
+    await decodeAndSave(payload.base64, process.env.AWS_CRED_DIR || sandbox, 'credentials');
     return res.sendStatus(200);
   } catch (e) {
     console.error(e);
@@ -30,7 +30,7 @@ const uploadGithubCredentials = async (req, res) => {
   console.log('/api/credentials/github');
   try {
     const payload = req.body;
-    await decodeAndSave(payload.base64, process.env.SSH_CRED_DIR || sandbox);
+    await decodeAndSave(payload.base64, process.env.SSH_CRED_DIR || sandbox, 'id_rsa');
     return res.sendStatus(200);
   } catch (e) {
     console.error(e);
